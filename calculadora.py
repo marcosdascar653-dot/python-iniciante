3f6e5a1591ce46ccbf05cc78a9583156714a2c40
marcosdascar653-dot <marcosdascar653@gmail.com>
2026-01-10 21:35:36 -0300

    Atualiza calculadora com menu e loop

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,33 @@
-print("=== Calculadora Simples ===")
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+while True:
+    print("\n=== Calculadora ===")
 
-print("Soma:", num1 + num2)
-print("Subtração:", num1 - num2)
-print("Multiplicação:", num1 * num2)
-print("Divisão:", num1 / num2)
+    n1 = float(input("Digite o primeiro número: "))
+    n2 = float(input("Digite o segundo número: "))
+
+    print("""
+1 - Soma
+2 - Subtração
+3 - Multiplicação
+4 - Divisão
+0 - Sair
+""")
+
+    op = input("Escolha uma opção: ")
+
+    if op == "0":
+        print("Programa encerrado.")
+        break
+    elif op == "1":
+        print("Resultado:", n1 + n2)
+    elif op == "2":
+        print("Resultado:", n1 - n2)
+    elif op == "3":
+        print("Resultado:", n1 * n2)
+    elif op == "4":
+        if n2 != 0:
+            print("Resultado:", n1 / n2)
+        else:
+            print("Erro: divisão por zero!")
+    else:
+        print("Opção inválida!")
